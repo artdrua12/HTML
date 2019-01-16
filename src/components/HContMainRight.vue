3abed546fc57a02a37e1f37ad028c99cbb4413d1
artdrua12 <artdrua@rambler.ru>
2019-01-16 14:05:28 +0300

    1

--- src/components/HContMainRight.vue
+++ src/components/HContMainRight.vue
@@ -10,37 +10,23 @@
         class="md-primary"
       >{{item}}</md-radio>
     </div>
-    <div class="table">
-      <!-- <tr>
+
+    <table>
+      <tr>
         <th></th>
         <th v-for="(item,key) in tableTitle" :key="key">{{item}}</th>
       </tr>
       <tr v-for="(item,key) in tableData" :key="key">
-        <td><md-radio v-if="item.Rate" v-model="radio2" :value="item.Ramp" class="md-primary"></md-radio></td>
+        <td>
+          <md-radio v-if="item.Rate" v-model="radio2" :value="item.Ramp" class="md-primary"></md-radio>
+        </td>
         <td>{{item.Ramp}}</td>
         <td>{{item.Rate}}</td>
         <td>{{item.Value}}</td>
         <td>{{item.Hold}}</td>
         <td>{{item.Run_Time}}</td>
-      </tr>-->
-      <md-table md-card>
-        <md-table-row>
-          <md-table-head></md-table-head>
-          <md-table-head v-for="(item,key) in tableTitle" :key="key">{{item}}</md-table-head>
-        </md-table-row>
-
-        <md-table-row v-for="(item,key) in tableData" :key="key">
-          <md-table-cell>
-            <md-radio v-if="item.Rate" v-model="radio2" :value="item.Ramp" class="md-primary"></md-radio>
-          </md-table-cell>
-          <md-table-cell>{{item.Ramp}}</md-table-cell>
-          <md-table-cell>{{item.Rate}}</md-table-cell>
-          <md-table-cell>{{item.Value}}</md-table-cell>
-          <md-table-cell>{{item.Hold}}</md-table-cell>
-          <md-table-cell>{{item.Hold}}</md-table-cell>
-        </md-table-row>
-      </md-table>
-    </div>
+      </tr>
+    </table>
   </div>
 </template>
 
@@ -60,7 +46,7 @@ export default {
       selected: [],
       tableTitle: [
         `Ramp##`,
-        `Rate,mL/min per min`,
+        `Rate,mL/ min per min`,
         `Value, mL/min`,
         `Hot Time,min`,
         `Run Time.min`
@@ -74,14 +60,14 @@ export default {
           Run_Time: null
         },
         {
-          Ramp: "Ramp1",
+          Ramp: "Ramp 1",
           Rate: 10,
           Value: 16.849,
           Hold: 0,
           Run_Time: 0
         },
         {
-          Ramp: "Ramp2",
+          Ramp: "Ramp 2",
           Rate: 10,
           Value: 16.849,
           Hold: 0,
@@ -104,8 +90,7 @@ export default {
   border-left: 1px solid gray;
 }
 .title {
-  padding: 10px;
-  width: 100%;
+  padding: 0px 12px;
 }
 .radioButt {
   display: flex;
@@ -113,23 +98,44 @@ export default {
   font-size: 12px;
   width: 80%;
 }
+.center {
+  text-align: center;
+}
+.md-radio {
+  margin: 0px;
+}
 div.table {
+  width: 100%;
   overflow: auto;
   font-size: 14px;
 }
 table {
   width: 100%; /* Ширина таблицы */
-  border: 4px double black; /* Рамка вокруг таблицы */
   border-collapse: collapse; /* Отображать только одинарные линии */
-}
-th,
-td {
-  border: 1px solid gray;
-  height: 10px;
-  width: 
+  text-align: right;
+  margin-top: 20px;
+  font-size: 14px;
 }
 th {
   background-color: #448aff;
   color: white;
+  text-align: center;
+  padding: 7px 2px;
+  font-size: 13px;
+  vertical-align: middle;
+}
+td {
+  border: 2px solid rgb(173, 167, 167);
+  padding: 5px 10px 0px 10px;
+  height: 30px;
+  vertical-align: middle;
+}
+
+td:first-child {
+  border-left: none;
+}
+
+td:last-child {
+  border-right: none;
 }
 </style>
